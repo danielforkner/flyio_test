311b2c266fc4b5ed644c02634c989031a54d548a
danielforkner <daniel.forkner@gmail.com>
2022-09-22 08:47:51 -0400

    init

---
+++ server/db/db.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  { host: process.env.DATABASE_HOSTNAME }
+);
+
+module.exports = db;
